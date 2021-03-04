c571a677b1fb573ca2932a72b9d3b9f0ea91810a
Max Zabarka <maxim.zabarka@gmail.com>
2021-03-04 12:27:19 -0700

    small fixes

--- src/background-scroll.js
+++ src/background-scroll.js
@@ -1,22 +1,17 @@
-let jumbotronWidth = document.querySelector(".jumbotron").clientWidth;
-let jumbotronHeight = document.querySelector(".jumbotron").clientHeight;
-
-const videoElement = document.querySelector("video");
-
-
-let lastScrollPos = window.pageYOffset
-let currentScrollPos = lastScrollPos
 
 if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  const videoElement = document.querySelector("video");
+
+  let lastScrollPos = window.scrollY
+  let currentScrollPos = lastScrollPos
+
   const scrollSpeed = 100
   const videoSpeed = 0.003
   function scrollPlay() {
-    currentScrollPos = window.pageYOffset || document.documentElement.scrollTop
+    currentScrollPos = window.scrollY || document.documentElement.scrollTop
     const difference = currentScrollPos - lastScrollPos
     lastScrollPos = currentScrollPos
-
     videoElement.currentTime = (videoElement.currentTime + (difference)/scrollSpeed + videoSpeed) % 60
-
     window.requestAnimationFrame(scrollPlay)
   }
   window.requestAnimationFrame(scrollPlay)
