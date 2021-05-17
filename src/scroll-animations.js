02365ad236fe218fedfffcf79fcca03f7216ff46
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-17 14:42:23 -0600

    added bars back and build

--- src/scroll-animations.js
+++ src/scroll-animations.js
@@ -1,16 +1,15 @@
 // import sal from "sal.js";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 
 import openAnimation from "./intro-animation.js";
 const button = document.querySelector(".jumbotron__content__button");
-const planet = document.getElementsByClassName('planet')[0]
-
+const planet = document.getElementsByClassName("planet")[0];
 
 window.onload = function () {
   setTimeout(() => {
     openAnimation(() => {
-      AOS.init({duration:2000,mirror:true,offset:120,once:true});
+      AOS.init({ duration: 2000, mirror: true, offset: 120, once: true });
       button.style.transition =
         getComputedStyle(document.querySelector(".jumbotron__content__button"))
           .transition +
@@ -20,9 +19,7 @@ window.onload = function () {
         getComputedStyle(document.querySelector(".jumbotron__content__button"))
           .transition
       );
-              planet.classList.add("planet-bright")
-
+      planet.classList.add("planet-bright");
     });
-  },1000)
-
+  }, 2000);
 };
